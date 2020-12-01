c7355187c417a79ada2e241bbe61b751ac0b4f1d
Sohaib Bitbucket <6sohaib@gmail.com>
2020-12-01 20:42:51 +0500

    Changed Calender and fixed

--- src/screens/AddAppointment.js
+++ src/screens/AddAppointment.js
@@ -3,7 +3,8 @@ import { Pressable, StyleSheet, Text, View } from 'react-native'
 import { Button, colors, Header, Icon } from 'react-native-elements';
 import { Portal, Searchbar, Provider, Surface, Divider } from 'react-native-paper';
 // import Calendar from 'react-native-calendar';
-import { Calendar } from 'react-native-calendario';
+// import { Calendar } from 'react-native-calendario';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 import { Avatar, Accessory } from 'react-native-elements';
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import InputStyle from "../components/InputStyle";
@@ -13,15 +14,27 @@ import { AntDesign } from '@expo/vector-icons';
 import { LogBox } from 'react-native';
 import Colors from '../config/colors';
 const uuid = require("react-native-uuid")
+// import { LocaleConfig } from 'react-native-calendars';
+
 // import moment from 'moment'
+
+
 
 
 export default function AddAppointment({ navigation, route }) {
     // This data will be given to backend
     // Will Decide More after selecting Calendar
+    //Using this one
+    //https://github.com/wix/react-native-calendars
     //https://github.com/amhinson/react-native-calendar
     //https://react-native-components.gitbook.io/calendar/
+
+
+
+
     // const [appointment, setappointmen] = useState("6/12/2019")
+
+
 
     const [appointmentTime, setappointmentTime] = useState("TAP TO CHANGE")
 
@@ -46,11 +59,27 @@ export default function AddAppointment({ navigation, route }) {
     //In finish appintment func check if appointmentTime not set 
     //set it to default, same as appointment details, 
     //or disable btn unless both are not set
-    const setDate = (d) => {
-        var ds = JSON.stringify(d.startDate)
-        var year = ds.substr(1, 4)
-        var month = ds.substr(6, 2)
-        var day = JSON.stringify(Number(ds.substr(9, 2)) + 1)
+
+    //Giving Date OBJ
+    const [dateobject, setdateobject] = useState("")
+
+    //Testing
+    const [selected, setselected] = useState("")
+
+
+
+    const setDate = (day) => {
+        // var ds = JSON.stringify(d.startDate)
+        // var year = ds.substr(1, 4)
+        // var month = ds.substr(6, 2)
+        // var day = JSON.stringify(Number(ds.substr(9, 2)) + 1)
+
+        console.log('selected day', day)
+        setselected(day.dateString)
+
+        var date_ob = new Date(day.timestamp);
+        setdateobject(date_ob)
+        console.log(date_ob, "dob");
 
         var weekday = new Array(7);
         weekday[0] = "Sunday";
@@ -61,7 +90,8 @@ export default function AddAppointment({ navigation, route }) {
         weekday[5] = "Friday";
         weekday[6] = "Saturday";
 
-        var weekn = weekday[d.startDate.getDay()];
+        var weekn = weekday[date_ob.getDay()];
+
 
         var month = new Array();
         month[0] = "January";
@@ -76,13 +106,21 @@ export default function AddAppointment({ navigation, route }) {
         month[9] = "October";
         month[10] = "November";
         month[11] = "December";
-        var monthn = month[d.startDate.getMonth()];
+        var monthn = month[date_ob.getMonth()];
 
         // setfulldate(year)
         setappointmentMonth(monthn)
-        setappointmentDate(day)
+        setappointmentDate(day.day)
         setappointmentWeekDay(weekn)
         setdoneDate(true)
+
+
+
+        // let copyOf = d.startDate.toDateString()
+        // // var x = new Date(copyOf)
+        // setdateobject(copyOf)
+
+        // console.log(copyOf, "as");
     }
 
     const handleAddAppointment = () => {
@@ -186,7 +224,91 @@ export default function AddAppointment({ navigation, route }) {
                 }}>SET DATE OF APPOINTMENT</Text>
 
                 <Surface style={{ height: 320, margin: 20 }}>
+
                     <Calendar
+                        markedDates={{
+                            [selected]: {
+                                selected: true,
+                                disableTouchEvent: true,
+                                // selectedColor: 'orange',
+                                // selectedTextColor: 'red'
+                            }
+                        }}
+
+                        // Initially visible month. Default = Date()
+                        // current={'2021-03-01'}
+                        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+                        // minDate={'2012-05-10'}
+                        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+                        // maxDate={'2012-05-30'}
+                        // Handler which gets executed on day press. Default = undefined
+                        onDayPress={(day) => { setDate(day) }}
+                        // Handler which gets executed on day long press. Default = undefined
+                        onDayLongPress={(day) => { console.log('selected day', day) }}
+                        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+                        monthFormat={'MMM yyyy'}
+                        // Handler which gets executed when visible month changes in calendar. Default = undefined
+                        onMonthChange={(month) => { console.log('month changed', month) }}
+                        // Hide month navigation arrows. Default = false
+                        //hideArrows={true}
+                        // Replace default arrows with custom ones (direction can be 'left' or 'right')
+                        // renderArrow={(direction) => (<Arrow />)}
+                        // Do not show days of other months in month page. Default = false
+                        //hideExtraDays={true}
+                        // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+                        // day from another month that is visible in calendar page. Default = false
+                        disableMonthChange={false}
+                        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+                        firstDay={1}
+                        // Hide day names. Default = false
+
+                        hideDayNames={false}
+
+                        // Show week numbers to the left. Default = false
+                        showWeekNumbers={true}
+                        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+                        onPressArrowLeft={subtractMonth => subtractMonth()}
+                        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+                        onPressArrowRight={addMonth => addMonth()}
+                        // Disable left arrow. Default = false
+                        // disableArrowLeft={false}
+                        // Disable right arrow. Default = false
+                        // disableArrowRight={false}
+                        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+                        disableAllTouchEventsForDisabledDays={true}
+
+                        theme={{
+                            backgroundColor: '#ffffff',
+                            calendarBackground: '#ffffff',
+                            textSectionTitleColor: '#b6c1cd',
+                            textSectionTitleDisabledColor: '#d9e1e8',
+                            selectedDayBackgroundColor: '#00adf5',
+                            selectedDayTextColor: '#ffffff',
+                            todayTextColor: '#00adf5',
+                            dayTextColor: '#2d4150',
+                            textDisabledColor: '#d9e1e8',
+                            dotColor: '#00adf5',
+                            selectedDotColor: '#ffffff',
+                            arrowColor: 'grey',
+                            disabledArrowColor: '#d9e1e8',
+                            monthTextColor: 'grey',
+                            //indicatorColor: 'blue',
+                            // textDayFontFamily: 'monospace',
+                            // textMonthFontFamily: 'monospace',
+                            // textDayHeaderFontFamily: 'monospace',
+                            textDayFontWeight: '300',
+                            textMonthFontWeight: 'bold',
+                            textDayHeaderFontWeight: '300',
+                            textDayFontSize: 16,
+                            textMonthFontSize: 16,
+                            textDayHeaderFontSize: 16
+                        }}
+                    // Replace default month and year title with custom one. the function receive a date as parameter.
+                    //renderHeader={(date) => {  }}
+                    // Enable the option to swipe between months. Default = false
+                    //enableSwipeMonths={true}
+                    />
+                    {/* <Calendar
                         nestedScrollEnabled={true}
                         numberOfMonths={2}
                         disableRange={true}
@@ -246,7 +368,7 @@ export default function AddAppointment({ navigation, route }) {
                             },
                             nonTouchableLastMonthDayTextStyle: {},
                         }}
-                    />
+                    /> */}
                 </Surface>
 
 
